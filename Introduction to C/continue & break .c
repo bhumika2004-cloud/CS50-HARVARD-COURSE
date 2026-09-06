0fcf2276209e2468dee8af10b97a2c572d6d78e6
Bhumika Khadka <khadkabhumika09@gmail.com>
2026-09-05 20:11:00 -0500

    Create continue & break .c
    continue goes back to the beginning of the loop and break moves to another loop.

---
+++ Introduction to C/continue & break .c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    while(true)
+    {
+        n = get_int("What's n?\n");
+        if (n<0)
+        {
+            continue;
+        }
+        else
+        {
+            break;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("meow\n");
+    }
+}
