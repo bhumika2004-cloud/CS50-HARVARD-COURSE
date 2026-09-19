74114e76034d4c7e5c946a0072cf2008b40d7d76
Bhumika Khadka <khadkabhumika09@gmail.com>
2026-09-18 21:55:37 -0500

    Add program to print 'meow' multiple times

---
+++ Introduction to C/taking no of times from user.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void meow(int times);
+
+int main(void)
+{
+    int n = get_int("What's n?\n");
+    meow(n);
+}
+
+
+void meow(int times)
+{
+  for (int i=0; i<times; i++)
+   {
+    printf("meow\n");
+   }
+}
